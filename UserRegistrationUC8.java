bdd37a6da8cb6865519aaf149cb4d6ad6067806b
rohit789@ <princeshmr27@gmail.com>
2023-08-16 11:57:46 +0530

    Check the Password Atleast One Upper Case & One Numaric & one special charcter

---
+++ UserRegistrationUC8.java
@@ -0,0 +1,29 @@
+package com.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationUC8 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter the Password Atleast One Upper Case & One Numaric & One Special Character:  ");
+		String passWord = scan.nextLine();
+		
+		String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
+		
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(passWord);
+		boolean res = m.matches();
+		
+		if(res) {
+			System.out.println("Password is Valid");
+		}
+		else {
+			System.out.println("Password is Invalid");
+		}
+
+	}
+
+}
